8a0a0df15ff716e6fc0bd321896ec251595340ea
Jeff Wainwright <1074042+yowainwright@users.noreply.github.com>
2026-08-26 01:13:32 +0000

    codependence 1.0.12

--- Formula/codependence.rb
+++ Formula/codependence.rb
@@ -1,8 +1,8 @@
 class Codependence < Formula
   desc "Enforce dependency version policy across projects, workspaces, and CI"
   homepage "https://jeffry.in/codependence/"
-  url "https://registry.npmjs.org/codependence/-/codependence-1.0.11.tgz"
-  sha256 "41b23f83f551206391c342e740a5d3e9613f4aadbe1cb1107ea1750ac4eaa940"
+  url "https://registry.npmjs.org/codependence/-/codependence-1.0.12.tgz"
+  sha256 "3d5d8487fd53862e4d1f94f692c155e2fc7547862c4d35b7ef8fe67fdff524ff"
   license "MIT"
 
   depends_on "node"
